04569ce2b3cf7fcdfea63433759c7f78ce2142de
sechs <philippe.buteau@skema.edu>
2023-09-22 16:28:08 +0200

    first commit

---
+++ src/data.ts
@@ -0,0 +1,52 @@
+export let data = {
+  mercedes: {
+    path: '/mercedes',
+    src: 'https://images.unsplash.com/photo-1666561305159-5198975bbaae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2815&q=80',
+    text: 'mercedes',
+  },
+  ford: {
+    path: '/ford',
+    src: 'https://images.unsplash.com/photo-1666460023412-9032385f9569?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
+    text: 'ford',
+  },
+  bmw: {
+    path: '/bmw',
+    src: 'https://images.unsplash.com/photo-1672828660910-3e6f3ab4a249?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2903&q=80',
+    text: 'bmw',
+  },
+  cadillac: {
+    path: '/cadillac',
+    src: 'https://images.unsplash.com/photo-1590880351453-72de6aaf030e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzR8fHZpbnRhZ2UlMjBjYXJ8ZW58MHwxfDB8fHww&auto=format&fit=crop&w=800&q=60',
+    text: 'cadillac',
+  },
+  porsche: {
+    path: '/porsche',
+    src: 'https://images.unsplash.com/photo-1661719123161-b78fdbe15785?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3027&q=80',
+    text: 'porsche',
+  },
+  triumph: {
+    path: '/triumph',
+    src: 'https://images.unsplash.com/photo-1637085031464-02edb87c48b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
+    text: 'cadillac',
+  },
+  golf: {
+    path: '/golf',
+    src: 'https://images.unsplash.com/photo-1562172395-1ddea5047501?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHZpbnRhZ2UlMjBnb2xmJTIwY2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+    text: 'golf gti',
+  },
+  buick: {
+    path: '/buick',
+    src: 'https://images.unsplash.com/photo-1636066633358-21d2d670c61a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2815&q=80',
+    text: 'buick',
+  },
+  chevrolet: {
+    path: '/chevrolet',
+    src: 'https://images.unsplash.com/photo-1582816640547-0a4b756ae997?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+    text: 'chevrolet',
+  },
+  mustang: {
+    path: '/mustang',
+    src: 'https://images.unsplash.com/photo-1651833619757-c66ef93498a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
+    text: 'ford mustang',
+  },
+};
